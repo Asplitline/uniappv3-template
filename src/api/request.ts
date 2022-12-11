a01fadd75dd3de4f91c858ddae33ff7ee0d85ba3
Asplitline <893666612@qq.com>
2022-12-11 23:02:07 +0800

    feat: api demo & utils

---
+++ src/api/request.ts
@@ -0,0 +1,57 @@
+const isDev = true
+export const requestUrl = isDev ? '/api' : 'http://localhost:8088'
+
+type REQUEST_TYPE = 'POST' | 'OPTIONS' | 'GET' | 'HEAD' | 'PUT' | 'DELETE' | 'TRACE' | 'CONNECT' | undefined
+
+interface RequestOption {
+  method: REQUEST_TYPE
+  url: string
+  data: any
+  showLoading: boolean
+  timeout: number
+  headers: any
+}
+
+export default ({ method = 'POST', url, data = {}, showLoading = false, timeout = 5000, headers = {} }: RequestOption) => {
+  return new Promise((resolve, reject) => {
+    if (showLoading) {
+      uni.showLoading({
+        title: '请求中...'
+      })
+    }
+    const token = uni.getStorageSync('token')
+
+    const requestOption = {
+      url: requestUrl + url,
+      method,
+      data,
+      header: {
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'access-auth-token': token,
+        ...headers
+      },
+      timeout
+    }
+
+    if (!token) {
+      delete requestOption.header['access-auth-token']
+    }
+    uni.request({
+      ...requestOption,
+      success: (res) => {
+        resolve(res.data)
+      },
+      fail: (res) => {
+        resolve({
+          code: 500,
+          message: res.errMsg
+        })
+      },
+      complete: (res) => {
+        if (showLoading) {
+          uni.hideLoading()
+        }
+      }
+    })
+  })
+}
---
+++ src/pages/login/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <view class="login">
+    <view class="p-20"> 
+
+      
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped></style>
