9bcb334f36be2fc103e6e9415b148a36c5a69298
Asplitline <893666612@qq.com>
2022-12-12 00:48:47 +0800

    chore: rebase

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,15 +1,26 @@
 <template>
   <cs-layout class="login">
     <pcs-navbar title="123213"></pcs-navbar>
-    <view class="p-20">
-      <u-badge type="success" class="badge" count="7"></u-badge>
-      <u-button>21312321</u-button>
+    <view class="p-4 w-10">
+      <u-form :model="formModel" ref="uForm" label-width="120">
+        <u-form-item><u-input v-model="formModel.username" placeholder="用户名" class="cs-input" /></u-form-item>
+        <u-form-item><u-input v-model="formModel.password" placeholder="密码" class="cs-input" /></u-form-item
+      ></u-form>
     </view>
   </cs-layout>
 </template>
 
 <script lang="ts" setup>
-// import CsLayout from '@/components/base/cs-layout/index.vue'
+import { reactive } from 'vue'
+
+const formModel = reactive<{ username?: string; password?: string }>({})
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.cs-input {
+  border-radius: 24rpx;
+  // padding: 10rpx 20rpx !important;
+  padding: 0 30rpx !important;
+  border: 1px solid #000;
+}
+</style>
