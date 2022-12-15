7d50b39a4e41ad1960d0265fb36b76838b65e2b5
Asplitline <893666612@qq.com>
2022-12-15 22:28:35 +0800

    fix: pcs-icon

--- src/components/packages/pcs-icon/index.vue
+++ src/components/packages/pcs-icon/index.vue
@@ -1,11 +1,18 @@
 <template>
   <view class="pcs-icon flex">
     <slot name="prefix"></slot>
-    <u-icon v-on="$attrs"> </u-icon>
+    <u-icon v-bind="$attrs" :size="props.size"> </u-icon>
     <slot></slot>
   </view>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const props = defineProps({
+  size: {
+    type: [String, Number],
+    default: 48
+  }
+})
+</script>
 
 <style lang="scss" scoped></style>
