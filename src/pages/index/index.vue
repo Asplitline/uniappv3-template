0541678b33d75303894149699eaba2bafc90bbf6
Asplitline <893666612@qq.com>
2022-12-17 00:18:52 +0800

    feat(page): adjust home

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,23 +1,58 @@
 <template>
-  <cs-layout class="content" hasTabbar>
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area bg-[#f00000] w-100 h-100">
-      <text class="text-34 color-white">1231231212</text>
+  <cs-layout class="content" :hasPadding="false" hasTabbar>
+    <view class="px-3 flex items-center bg-white py-3 b-b-1 b-gray-100">
+      <image
+        class="w-80 h-80 b-rd-50%"
+        src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/522.jpg"
+      ></image>
+      <text class="text-32 flex-1 ml-2">用户A</text>
+      <image src="@/static/icons/more.png" class="w-54 h-54"></image>
     </view>
-    <view class="shadow w-150 h-150 mt-20 b-red b-2"> </view>
-    <text class="title" @click="skip">{{ title }}</text>
-    <pcs-icon name="/static/icons/info.png"></pcs-icon>
+    <view class="">
+      <text>当前进度</text>
+      <cs-steps :active="active">
+        <cs-step>
+          <template #prefix>12321</template>
+          <view>
+            <view>21321</view>
+            <view>21321</view>
+            <view>21321</view>
+            <view>21321</view>
+            <view>21321</view>
+          </view>
+        </cs-step>
+        <cs-step>
+          <template #prefix>12321</template>
+        </cs-step>
+        <cs-step>
+          <template #prefix>12321</template>
+        </cs-step>
+        <cs-step>
+          <template #prefix>12321</template>
+        </cs-step>
+        <cs-step>
+          <template #prefix>12321</template>
+        </cs-step>
+      </cs-steps>
+    </view>
+    <button @click="next">next</button>
   </cs-layout>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-const title = ref('Hello')
+const active = ref(0)
 const skip = () => {
   uni.navigateTo({
     url: '/pages/login/index'
   })
 }
+const next = () => {
+  active.value++
+  if (active.value > 5) {
+    active.value = 0
+  }
+}
 </script>
 
-<style></style>
+<style lang="scss" scoped></style>
