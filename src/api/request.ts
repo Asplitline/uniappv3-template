14e1171232550cf78bf9ee1129a71bca6d554115
Asplitline <893666612@qq.com>
2022-12-17 21:24:37 +0800

    build(api): api demo & proxy

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,41 +1,40 @@
 const isDev = true
-export const requestUrl = isDev ? '/api' : 'http://localhost:8088'
+export const requestUrl = isDev ? '/api' : 'http://localhost:8090'
 
 type REQUEST_TYPE = 'POST' | 'OPTIONS' | 'GET' | 'HEAD' | 'PUT' | 'DELETE' | 'TRACE' | 'CONNECT' | undefined
 
 interface RequestOption {
-  method: REQUEST_TYPE
-  url: string
-  data: any
-  showLoading: boolean
-  timeout: number
-  headers: any
+  method?: REQUEST_TYPE
+  data?: any
+  showLoading?: boolean
+  timeout?: number
+  headers?: any
 }
 
-export default ({ method = 'POST', url, data = {}, showLoading = false, timeout = 5000, headers = {} }: RequestOption) => {
+export default (url: string, { method = 'GET', data = {}, showLoading = false, timeout = 5000, headers = {} }: RequestOption) => {
   return new Promise((resolve, reject) => {
     if (showLoading) {
       uni.showLoading({
         title: '请求中...'
       })
     }
-    const token = uni.getStorageSync('token')
+    // const token = uni.getStorageSync('token')
 
     const requestOption = {
       url: requestUrl + url,
       method,
       data,
       header: {
-        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'access-auth-token': token,
+        // 'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        // 'access-auth-token': token,
         ...headers
       },
       timeout
     }
 
-    if (!token) {
-      delete requestOption.header['access-auth-token']
-    }
+    // if (!token) {
+    //   delete requestOption.header['access-auth-token']
+    // }
     uni.request({
       ...requestOption,
       success: (res) => {
@@ -47,7 +46,7 @@ export default ({ method = 'POST', url, data = {}, showLoading = false, timeout 
           message: res.errMsg
         })
       },
-      complete: (res) => {
+      complete: () => {
         if (showLoading) {
           uni.hideLoading()
         }
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,15 @@ export default defineConfig({
   plugins: [
     uni(),
     // https://github.com/antfu/unocss
-    Unocss(),
+    Unocss()
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8090/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
