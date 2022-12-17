95eb3debc5a136094d42a1ce485e8f2664c949a3
Asplitline <893666612@qq.com>
2022-12-17 23:37:51 +0800

    build: import pinia & Cache utils

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import 'uno.css'
 import uView from 'vk-uview-ui'
 import { createSSRApp } from 'vue'
 import App from './App.vue'
+import pinia from '@/store'
+
 export function createApp() {
   const app = createSSRApp(App)
+  app.use(pinia)
   app.use(uView)
   return {
     app
