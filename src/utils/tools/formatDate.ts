243c6696b29291a34e4b1e93293003ffa2e6a74c
Asplitline <893666612@qq.com>
2022-12-18 16:41:43 +0800

    build(utils): timeago & formatDate

---
+++ src/utils/tools/formatDate.ts
@@ -0,0 +1,25 @@
+/**
+ * @name formatDate
+ *
+ * @param t
+ * @param format
+ * @returns
+ */
+export default function (t: any, format: string = 'YYYY/MM/DD') {
+  let date = t ? new Date(t) : new Date(),
+    Y = date.getFullYear() + '',
+    M = date.getMonth() + 1,
+    D = date.getDate(),
+    H = date.getHours(),
+    m = date.getMinutes(),
+    s = date.getSeconds()
+  if (date.toString() === 'Invalid Date') return '-/-'
+  return format
+    .replace(/YYYY|yyyy/g, Y)
+    .replace(/YY|yy/g, Y.substr(2, 2))
+    .replace(/MM/g, (M < 10 ? '0' : '') + M)
+    .replace(/DD|dd/g, (D < 10 ? '0' : '') + D)
+    .replace(/HH|hh/g, (H < 10 ? '0' : '') + H)
+    .replace(/mm/g, (m < 10 ? '0' : '') + m)
+    .replace(/ss/g, (s < 10 ? '0' : '') + s)
+}
